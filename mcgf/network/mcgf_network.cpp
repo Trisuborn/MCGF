b0d58b88b0130405f05b110589ebfdff25df61d5
Trisuborn <553070703@qq.com>
2021-02-17 05:33:42 +0800

    add mcgf_network files

---
+++ mcgf/network/mcgf_network.cpp
@@ -0,0 +1,11 @@
+/************************************************
+ * @file mcgf_network.cpp
+ * @author Trisuborn (ttowfive@gmail.com)
+ * @brief
+ * @version 1.0
+ * @date 2021-02-17
+ *
+ * @copyright Copyright (c) 2021
+ *
+ *************************************************/
+#include "mcgf_network.h"
---
+++ mcgf/network/mcgf_network.h
@@ -0,0 +1,6 @@
+#ifndef MCGF_NETWORK_H
+#define MCGF_NETWORK_H
+
+
+
+#endif // MCGF_NETWORK_H
